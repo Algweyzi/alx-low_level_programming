2dc0945f041c613443459de356154ec9746b0cd5
Algweyzi <nebilnasir24@gmail.com>
2023-08-10 15:00:40 -0700

    same

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<studio.h>
 /**
- * main -entry point 
+ * main - Entry point 
  *
- * Return: always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	puts(''\''Programming is like building a multilingual puzzle'');
-	return (0);
+        return (0);
 }
